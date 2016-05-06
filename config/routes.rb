9730b8a061c212e95055343b73f41ae2e930234c
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-06 02:15:39 +0100

    chore (controllers): generate sessions controller to handle log-in and log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
+    namespace :v1 do
+      post "/auth/login" => "sessions#create"
+      get "/auth/logut" => "sessions#destroy"
+    end
+  end
+
+  namespace :api do
     resources :users, only: [:show, :create, :update, :destroy]
   end
 
