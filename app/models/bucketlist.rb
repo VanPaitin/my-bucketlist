39e8b16c745854db391d72d9deb3f83daa37b9b6
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-10 01:50:41 +0100

    feat (search): implement search with name [Delivers #119136465]

--- app/models/bucketlist.rb
+++ app/models/bucketlist.rb
@@ -3,8 +3,4 @@ class Bucketlist < ActiveRecord::Base
   belongs_to :user
   has_many :items, dependent: :destroy
   validates :name, length: { minimum: 2, maximimu: 20 }
-  def self.paginate(page = 1, records = 20)
-    offset_parameter = (page - 1) * records
-    offset(offset_parameter).limit(records)
-  end
 end
