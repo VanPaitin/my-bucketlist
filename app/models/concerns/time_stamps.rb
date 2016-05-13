189d2db07d2ff9177f0339a1c622628f4e1eb2fb
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-13 04:07:21 +0100

    chore (concerns): properly format the date and time display [Starts #119158295]

--- app/models/concerns/time_stamps.rb
+++ app/models/concerns/time_stamps.rb
@@ -1,9 +1,9 @@
 module TimeStamps
   def date_created
-    created_at.strftime("#{created_at.day.ordinalize} %B %Y")
+    created_at.strftime("%A, #{created_at.day.ordinalize} %B %Y at %I:%M %p")
   end
 
   def date_modified
-    updated_at.strftime("#{updated_at.day.ordinalize} %B %Y")
+    updated_at.strftime("%A, #{updated_at.day.ordinalize} %B %Y at %I:%M %p")
   end
 end
