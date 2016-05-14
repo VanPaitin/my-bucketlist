3b292d1aca78febdb7303c3bb5282feeb84776f7
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-14 04:29:07 +0100

    chore (tests): write test for the items destroy end point

--- test/integration/item/update_item_test.rb
+++ test/integration/item/update_item_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Item::UpdateItemTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @bucketlist = create(:bucketlist, user_id: @user.id)
+    @item = create(:item, bucketlist_id: @bucketlist.id, done: false)
+  end
+  test "the truth" do
+
+  end
 end
