7cc13f7ae8cbcdda90c4c4a62a10ec53ab6ee240
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-26 08:05:14 +0100

    chore (test): remove redundant test in user model test and restructure bucketlist search to use bucketlist factory

--- test/factories/bucketlist.rb
+++ test/factories/bucketlist.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :bucketlist do
-    sequence(:name) { |n| Faker::Commerce.department + " #{n}" }
+    sequence(:name) { |n| Faker::Commerce.department + " list#{n}" }
   end
 end
--- test/models/bucketlist_test.rb
+++ test/models/bucketlist_test.rb
@@ -3,10 +3,10 @@ require "test_helper"
 class BucketlistTest < ActiveSupport::TestCase
   setup do
     @user = create(:user)
-    @bucketlist = @user.bucketlists.create(name: "bukola")
-    @bucketlist2 = @user.bucketlists.create(name: "firstLIST")
-    3.times { @user.bucketlists.create(name: "fakerLisT#{rand(100)}") }
+    @bucketlist = create(:bucketlist, name: "bukola", user_id: @user.id)
+    @bucketlists = create_list(:bucketlist, 3, user_id: @user.id)
   end
+
   should belong_to(:user)
   should have_many(:items).dependent(:destroy)
   should validate_presence_of(:name)
@@ -14,11 +14,11 @@ class BucketlistTest < ActiveSupport::TestCase
   should validate_uniqueness_of(:name).case_insensitive
 
   test "can return the result of a given search term" do
-    assert_equal 5, @user.bucketlists.count
-    assert_equal 4, Bucketlist.search(@user, "list").count
-    refute_includes Bucketlist.search(@user, "list").
-      map(&:name), @bucketlist.name
-    assert_includes Bucketlist.search(@user, "list").
-      map(&:name), @bucketlist2.name
+    assert_equal 4, @user.bucketlists.count
+    search_result = Bucketlist.search(@user, "list")
+    assert_equal 3, search_result.count
+    refute_includes search_result.map(&:name), @bucketlist.name
+    assert_equal search_result.count, @bucketlists.count
+    assert @bucketlists.all? { |bucketlist| search_result.include? bucketlist }
   end
 end
