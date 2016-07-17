bb5460884ef9a9aa03694c75965494557f304fca
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-17 23:51:30 +0100

    comment out invalid routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include ErrorHandling
 
-
-
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
