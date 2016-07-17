015362e8d4559de7a673a240381e8a4584e9c3f4
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-18 00:17:22 +0100

    feat: add rack-cors configuration to application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module MyBucketlist
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'localhost:8080'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
