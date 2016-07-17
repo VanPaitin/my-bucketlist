8d72072f1f5656ac178ecfb47ab20c859b4ccd68
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-17 23:43:27 +0100

    chore(controller): edited the application controller to support cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::API
   before_action :add_allow_credentials_headers
   before_action :ensure_login, except: :invalid_endpoint
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   include ActionController::Serialization
   include ErrorHandling
+
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+    end
+  end
 
   private
 
