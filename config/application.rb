2ed72c2c0242496fa7dd0f9b54256d8c11fab4b4
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-19 00:02:01 +0100

    feat (config): include put and delete methods in array of allowed methods

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module MyBucketlist
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins 'localhost:8080'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
     config.active_record.raise_in_transactional_callbacks = true
